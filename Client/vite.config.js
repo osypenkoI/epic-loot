556bf621fcf05942d90eb0ea7f32d38aefea05a8
osypenkoI <127287572+osypenkoI@users.noreply.github.com>
2024-12-04 14:11:27 +0200

    Update vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-server: {
-server: {
+  server: {
     proxy: {
         '/api': 'https://epic-loot-backend-production.up.railway.app:8080',
     },
