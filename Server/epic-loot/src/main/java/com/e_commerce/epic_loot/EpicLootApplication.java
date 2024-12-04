2a1fde961d5b46f076202b7d2f4a070a79cb4928
osypenkoI <127287572+osypenkoI@users.noreply.github.com>
2024-12-04 15:55:24 +0200

    Update EpicLootApplication.java

--- Server/epic-loot/src/main/java/com/e_commerce/epic_loot/EpicLootApplication.java
+++ Server/epic-loot/src/main/java/com/e_commerce/epic_loot/EpicLootApplication.java
@@ -2,26 +2,12 @@ package com.e_commerce.epic_loot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EpicLootApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EpicLootApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EpicLootApplication.class, args);
+	}
 
-    @Configuration
-    public class WebConfig implements WebMvcConfigurer {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                    .allowedOrigins("https://epic-loot.netlify.app/") // URL вашого фронту
-                    .allowedMethods("GET", "POST", "PUT", "DELETE") // Методи, які дозволяються
-                    .allowCredentials(true); // Якщо вам потрібні куки або автентифікація
-        }
-    }
 }
